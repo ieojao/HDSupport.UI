7116be77e8509f0be58d93d7e3947dbbca10760f
gicarmox <nottozlima@gmail.com>
2025-06-26 17:02:00 -0300

    correação side bar

--- src/app/usuarios/page.tsx
+++ src/app/usuarios/page.tsx
@@ -1,14 +1,10 @@
 "use client"
-import Header from "@/components/layout/header";
 import UserTable from "@/components/usuarios/UserTable";
 
 export default function Page(){
     return(
-        <div className="flex min-h-screen w-full">
-            <Header/>
-            <div className="flex-1 flex flex-col w-full">
-                <UserTable/>
-            </div>
+        <div className="flex-1 flex flex-col w-full">
+            <UserTable/>
         </div>
     )
 }
